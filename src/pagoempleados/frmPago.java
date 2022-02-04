37c69a16611c73db39aac27ef06db47e0f10921e
Salvador <slb_18@hotmail.com>
2022-02-04 03:17:15 -0600

    Formulario Terminado en Strix

--- src/pagoempleados/frmPago.java
+++ src/pagoempleados/frmPago.java
@@ -6,9 +6,12 @@
 package pagoempleados;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.text.NumberFormat;
+//import java.event.*;
 /**
  *
- * @author ChavaWS
+ * @author Salvador Lopez Baltazar
  */
 public class frmPago extends javax.swing.JFrame {
 
@@ -30,11 +33,21 @@ public class frmPago extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         cmdSalir = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtArea = new javax.swing.JTextArea();
+        cmdLimpiar = new javax.swing.JButton();
+        cmdMostrar = new javax.swing.JButton();
+        txtNombre = new javax.swing.JTextField();
+        txtHoras = new javax.swing.JTextField();
+        txtTarifa = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("PAGO DE EMPLEADOS");
 
-        jLabel1.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
         jLabel1.setText("PAGO DE EMPLEADOS");
 
         cmdSalir.setText("Salir");
@@ -44,25 +57,96 @@ public class frmPago extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setText("Nombre");
+
+        jLabel3.setText("Horas");
+
+        jLabel4.setText("Tarifa");
+
+        txtArea.setColumns(20);
+        txtArea.setRows(5);
+        jScrollPane1.setViewportView(txtArea);
+
+        cmdLimpiar.setText("Limpiar");
+        cmdLimpiar.setName("cmdLimpiar"); // NOI18N
+        cmdLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdLimpiarActionPerformed(evt);
+            }
+        });
+
+        cmdMostrar.setText("Mostrar");
+        cmdMostrar.setName("cmdMostrar"); // NOI18N
+        cmdMostrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdMostrarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(127, 127, 127)
-                .addComponent(jLabel1)
-                .addContainerGap(120, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(cmdSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(cmdMostrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(cmdSalir, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                            .addComponent(cmdLimpiar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(15, 15, 15))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(37, 37, 37)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtNombre)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(txtHoras, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addGap(159, 159, 159)
+                                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(txtTarifa, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addGap(0, 190, Short.MAX_VALUE)))))))
+                .addGap(15, 15, 15))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(193, 193, 193))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(14, 14, 14)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 220, Short.MAX_VALUE)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(txtHoras, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel4)
+                    .addComponent(txtTarifa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(cmdMostrar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(cmdLimpiar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(cmdSalir)
                 .addGap(18, 18, 18))
         );
@@ -72,13 +156,67 @@ public class frmPago extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cmdSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdSalirActionPerformed
-        // Salir del Programa
+        // Mensaje y Salir del Programa
          JOptionPane.showMessageDialog(null,"Hasta Luego","Sistema",JOptionPane.WARNING_MESSAGE);
         
          System.exit(0);
 
     }//GEN-LAST:event_cmdSalirActionPerformed
 
+    private void cmdMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdMostrarActionPerformed
+        // TODO add your handling code here:
+            //Entrada
+            int horas = Integer.parseInt(txtHoras.getText());
+            double tarifa = Double.parseDouble(txtTarifa.getText());
+            
+            //Proceso
+            double basico = horas * tarifa;
+            double bonificacion = basico * 0.2;
+            double neto = (basico + bonificacion ) * 0.9;
+               
+            //Salida
+            NumberFormat importe = NumberFormat.getCurrencyInstance(); //Sirve para comvertir a $ las cantidades
+            txtArea.setText(" **Resumen de Pago a Empleados** ");
+            txtArea.append("\n ----------------------------------------------");
+            txtArea.append("\n ----------------------------------------------");
+            txtArea.append("\n El basico es: \t \t" + (importe.format(basico)));
+            txtArea.append("\n La bonificacion es: \t" + (importe.format(bonificacion)));
+            txtArea.append("\n ----------------------------------------------");
+            txtArea.append("\n El Neto es:  \t \t" + (importe.format(neto)));
+            //txtArea.append("\n El Neto es: $ \t \t" + String.format("%.2f", neto));
+            
+            JOptionPane.showMessageDialog(null,"Procesado....");
+            
+    }//GEN-LAST:event_cmdMostrarActionPerformed
+
+    private void cmdLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdLimpiarActionPerformed
+        // TODO add your handling code here:
+            //Limpiar
+            txtArea.setText("");
+            txtHoras.setText("");
+            txtNombre.setText("");
+            txtTarifa.setText("");
+            
+            txtNombre.requestFocus();
+    }//GEN-LAST:event_cmdLimpiarActionPerformed
+    /*public void actionPerformed(ActionEvent e){
+        if (e.getSource()==cmdMostrar){
+            
+            //Entrada
+            int horas = Integer.parseInt(txtHoras.getText());
+            double tarifa = Double.parseDouble(txtTarifa.getText());
+            
+            //Proceso
+            double basico = horas * tarifa;
+            double bonificacion = basico * 0.2;
+            double neto = (basico + bonificacion ) * 0.9;
+            
+            //Salida
+            txtNombre.setText("hola");
+            txtArea.setText(" **Resumen de Pago a Empleados** ");
+            
+    }
+    }/*
     /**
      * @param args the command line arguments
      */
@@ -115,7 +253,17 @@ public class frmPago extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmdLimpiar;
+    private javax.swing.JButton cmdMostrar;
     private javax.swing.JButton cmdSalir;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea txtArea;
+    private javax.swing.JTextField txtHoras;
+    private javax.swing.JTextField txtNombre;
+    private javax.swing.JTextField txtTarifa;
     // End of variables declaration//GEN-END:variables
 }
