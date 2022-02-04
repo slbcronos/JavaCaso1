c37ececb6e1f7de53fd28ff5fd300234050e91bf
Salvador LB <slb_18@hotmail.com>
2022-02-03 21:48:46 -0600

    Frame en Mac

---
+++ src/pagoempleados/PagoEmpleados.java
@@ -0,0 +1,24 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pagoempleados;
+
+/**
+ *
+ * @author Salvador Lopez Baltazar
+ */
+public class PagoEmpleados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ARRANQUE DEL FORMULARIO
+        
+        frmPago fraVista = new frmPago();
+        fraVista.setVisible(true);
+    }
+    
+}
